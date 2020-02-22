8c9bc6615fe71b4cd3b420feb85ab50de55269b6
Blackscure <Wekesabuyahi@gmail.com>
2020-02-22 10:16:50 +0300

    script folder

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+//what we do section //
+
+$(document).ready(function () {
+    $(".design_icon").click(function () {
+        $("#design-p").toggle();
+    });
+
+    $(".dev_icon").click(function () {
+        $("#development").toggle();
+    });
+    
+    $(".product_icon").click(function () {
+        $("#pmanagent").toggle();
+    });
+});
