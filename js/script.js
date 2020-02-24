7326a0321d20f866b9a36ef789dfb01d04670da7
Blackscure <Wekesabuyahi@gmail.com>
2020-02-24 14:43:13 +0300

    contact modified

--- js/script.js
+++ js/script.js
@@ -13,8 +13,16 @@ $(".product").click(function(){
     $("#product").toggle();
  });
 
- $("form").submit(function(){
-    alert("You have submitted successfully")
-});
+  
+
+ $("#submit").click(function (event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var mail = $("mail").val();
+    var text = $("#text").val();
+    //var key = '43ab10333d46e19325f96d210ed40c49-us5';//
+    if (name == "" || mail == "" || text == "") {
+        alert("hi" + " " + name + " " + "we have recieved Your message .Thank you for reaching out to us")        } else {
+        alert("please make sure you have filled in the form correctly")        }    })
 
 
